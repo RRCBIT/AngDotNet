ec51e714281849cc3b543a7cd26bbcc87271603a
Y-Chi Lu <luyege+github@gmail.com>
2016-05-31 23:56:36 -0500

    App.js

---
+++ Client/Scripts/AtTheMovies.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+
+    var config = function ($routeProvider) {
+        $routeProvider
+            .when("/list", { templateUrl: "/client/views/list.html" })
+            .when("/li", { templateUrl: "/client/views/li.html" })
+            .when("/details/:id", { templateUrl: "/client/views/details.html" })
+            .otherwise({ redirectTo: "/li" });
+    };
+    config.$inject = ["$routeProvider"];
+
+    app.config(config);
+    app.constant("movieApiUrl", "/api/movies/");
+
+}(angular.module("AtTheMovies", ["ngRoute", "ngResource", "ngAnimate"])));
