92a8026c47e98047edaa03aee94fe3607af8622e
Y-Chi Lu <luyege+github@gmail.com>
2016-05-31 23:50:20 -0500

    1st file

---
+++ Client/Scripts/LiController.js
@@ -0,0 +1,10 @@
+(function(app) {
+//var app=angular.module("AtTheMovies");
+var LiController = function($scope){
+ $scope.message="H,M";
+};
+
+
+app.controller("LiController",LiController);
+
+}(angular.module("AtTheMovies")));
