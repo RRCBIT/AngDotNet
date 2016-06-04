676f8e138535bb8b725bc02116dd5c72190d45e7
Y-Chi Lu <luyege+github@gmail.com>
2016-06-04 01:34:38 -0500

    detail service

--- Client/Scripts/DetailController.js
+++ Client/Scripts/DetailController.js
@@ -1,9 +1,11 @@
 (function(app) {
     
-var DetailController=function($scope,$http,$routeParams)  {
+var DetailController=function($scope,$http,$routeParams,movieService)  {
     var id= $routeParams.id;
     
-    $http.get("/api/movies/"+id)
+    //$http.get("/api/movies/"+id)
+    movieService
+    .getById(id)
     .success(function(data){
        $scope.movie = data;
     });
