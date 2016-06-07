28d2eb704c971b2591e5f45d6b1ed5c6c7f79174
Y-Chi Lu <luyege+github@gmail.com>
2016-06-06 19:09:08 -0500

    delte

--- Client/Scripts/LiController.js
+++ Client/Scripts/LiController.js
@@ -1,13 +1,32 @@
 (function(app) {
 //var app=angular.module("AtTheMovies");
+
+
 var LiController = function($scope,$http,movieService){
     //$http.get("api/movies")
     movieService
         .getAll()
         .success(function (data) {
             $scope.movies=data;
-        })
+        });
+        
+        $scope.delete = function (movie) {
+            movieService.delete(movie)
+                .success(function() {
+                    removeMovieById(movie.id);
+                });
+        };
+        
+        var removeMovieById = function(id) {
+            for (var i = 0; i < $scope.movies.length; i++) {
+                if ($scope.movies[i].id == id) {
+                    $scope.movies.splice(i, 1);
+                }
+            }
+        };        
  $scope.message="HelloH,M";
+ 
+ 
 };
 
 app.controller("LiController",LiController);
