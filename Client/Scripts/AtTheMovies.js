e58de9ffe9d7d8d8edb4c554b2c2425867029a30
Y-Chi Lu <luyege+github@gmail.com>
2016-06-06 19:31:23 -0500

    t

--- Client/Scripts/AtTheMovies.js
+++ Client/Scripts/AtTheMovies.js
@@ -5,6 +5,7 @@
             .when("/list", { templateUrl: "/client/views/list.html" })
             .when("/li", { templateUrl: "/client/views/li.html" })
             .when("/details/:id", { templateUrl: "/client/views/details.html" })
+            .when("/detail/:id", { templateUrl: "/client/views/detail.html" })
             .otherwise({ redirectTo: "/li" });
     };
     config.$inject = ["$routeProvider"];
